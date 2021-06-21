2e70f81db3bcb3a5624e3fe7a2000cd243b212e2
Goh Li Ying <gohly2210@gmail.com>
2021-06-21 23:22:58 +0800

    Simple draft of add new user

---
+++ Health4U(Admin)/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Health4U_Admin_.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult AddNewUser()
+        {
+            return View();
+        }
+    }
+}
