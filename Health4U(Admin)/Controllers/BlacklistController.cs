c01004effe61c08021bb3bdccc5ed8d8db69cc18
2008937@m365.tarc.edu.my <2008937@m365.tarc.edu.my>
2021-06-30 17:03:49 +0800

    Blacklist/Add Blacklist

---
+++ Health4U(Admin)/Controllers/BlacklistController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Health4U_Admin_.Controllers
+{
+    public class BlacklistController : Controller
+    {
+        // GET: Blacklist
+        public ActionResult Blacklist()
+        {
+            return View();
+        }
+
+        // GET: Blacklist
+        public ActionResult AddBlacklist()
+        {
+            return View();
+        }
+    }
+}
