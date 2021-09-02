b5cc653875db8dd71fb298cd628f108f29a1b3d4
2008937@m365.tarc.edu.my <2008937@m365.tarc.edu.my>
2021-09-02 21:19:00 +0800

    Latest

---
+++ Health4U(Admin)/Controllers/DiseaseController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Health4U_Admin_.Controllers
+{
+    public class DiseaseController : Controller
+    {
+        // GET: Disease
+        public ActionResult Disease()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
